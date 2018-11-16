41ae81508cb44126259c6421ece3c447a181bb50
Lance Hamilton <lance.hamilton24@gmail.com>
2018-11-15 19:05:21 -0600

    added firebase api keys

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
 
+import apiKeys from '../db/apiKeys.json';
+
+import loginButton from '../components/Auth/auth';
+
 $('#to-do').html('hello world');
+
+const initializeApp = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+};
+
+initializeApp();
